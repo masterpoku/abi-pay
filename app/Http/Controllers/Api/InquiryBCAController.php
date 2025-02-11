b084c5fd075584397c7567641986fedf7cd9daee
masterpoku <wafitakputusasa@gmail.com>
2025-02-11 14:21:16 +0700

    BCA

--- app/Http/Controllers/Api/InquiryBCAController.php
+++ app/Http/Controllers/Api/InquiryBCAController.php
@@ -431,6 +431,9 @@ class InquiryBCAController extends Controller
         $totalAmount = isset($validated['totalAmount']['value']) ? (float)$validated['totalAmount']['value'] : 0;
         $nominalTagihan = (float)$user_data->nominal_tagihan;
     
+        Log::info('paidAmount: '.$paidAmount);
+        Log::info('nominalTagihan: '.$nominalTagihan);
+        Log::info('totalAmount: '.$totalAmount);
         if ($paidAmount !== $nominalTagihan || $totalAmount !== $nominalTagihan) {
             return response()->json([
                 "responseCode" => "4042513",
