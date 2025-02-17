05becc7fe5907ba8bbafe46f0869fb1d3bd2fe8e
masterpoku <wafitakputusasa@gmail.com>
2025-02-17 15:08:51 +0700

    BCA

--- app/Http/Controllers/Api/PaymentBCAController.php
+++ app/Http/Controllers/Api/PaymentBCAController.php
@@ -207,9 +207,9 @@ EOF;
             $partnerId = $request->header('X-PARTNER-ID');
             $today = now()->toDateString();
     
-            // if (!$this->validateServiceSignature($clientSecret, $method, $url, $authToken, $isoTime, $bodyToHash, $signature)) {
-            //     return response()->json(["responseCode" => "4012500", "responseMessage" => "Unauthorized. [Signature]"], 401);
-            // }
+            if (!$this->validateServiceSignature($clientSecret, $method, $url, $authToken, $isoTime, $bodyToHash, $signature)) {
+                return response()->json(["responseCode" => "4012500", "responseMessage" => "Unauthorized. [Signature]"], 401);
+            }
     
             if (!$this->validateHeaders($authToken, $clientSecret, $method, $url, $isoTime, $bodyToHash, $signature)) {
                 return response()->json(["responseCode" => "4012501", "responseMessage" => "Invalid Token (B2B)"], 401);
