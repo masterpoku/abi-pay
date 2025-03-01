c53df7bd1834e699a72ceb6f1086bb14e9c72fa1
masterpoku <wafitakputusasa@gmail.com>
2025-03-01 12:55:05 +0700

    BSI

--- app/Http/Controllers/Api/InquiryController.php
+++ app/Http/Controllers/Api/InquiryController.php
@@ -20,7 +20,7 @@ class InquiryController extends Controller
 
     public function __construct()
     {
-        $this->secret_key = env('SECRET_KEY', '1!jK%5XGX-M0)8_NIXb1Ldjj{u2>9L');
+        $this->secret_key = env('SECRET_KEY');
         $this->allowed_collecting_agents = ['BSM'];
         $this->allowed_channels = ['TELLER', 'IBANK', 'ATM', 'MBANK', 'FLAGGING'];
     }
